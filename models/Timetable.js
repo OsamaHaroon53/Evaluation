be23902010f374a69d6ddf33f926d0b50a74b204
OsamaHaroon53 <sonijaosama@gmail.com>
2018-12-20 19:58:31 +0500

    Time table schema added

---
+++ models/Timetable.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const TTSchema = new Schema({
+    Day: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 6
+    },
+    shift: {
+        type: String,
+        required: true
+    },
+    section: {
+        type: String,
+        required: true
+    },
+    Time: {
+        type: String,
+        required: true
+    },
+    
+    course: {
+        type: Schema.Types.ObjectId,
+        ref: 'course',
+        required: true
+    },
+    teacher: {
+        type: Schema.Types.ObjectId,
+        ref: 'teacher',
+        required: true
+    },
+    labTime: {
+        type: String
+    },
+    labTeacher: {
+        type: Schema.Types.ObjectId,
+        ref: 'teacher'
+    }
+});
+
+module.exports = mongoose.model('course', courseSchema);
