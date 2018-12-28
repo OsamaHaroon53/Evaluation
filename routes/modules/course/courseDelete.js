17592e5c3052888731b3586abf7c9c39dc5f6b3c
OsamaHaroon53 <sonijaosama@gmail.com>
2018-12-28 18:05:22 +0500

    Updae 'Deelete course api'

--- routes/modules/course/courseDelete.js
+++ routes/modules/course/courseDelete.js
@@ -3,11 +3,27 @@ const courseModel = require('../../../models/Course');
 module.exports = (req, res) => {
     courseModel.findByIdAndDelete(req.params.id)
         .then(data => {
-            console.log(data);
-            res.status(200).send('Deleted')
+            data ? res.status(200).send({
+                status: 200,
+                msg: 'Success: Data Delete succesfully'
+            }) : res.send({
+                status: 304,
+                msg: 'Fail: Data not found'
+            });
         })
         .catch(err => {
-            console.log('err: ' + err);
-            res.status(403).send(err);
+            if (err['name'] && err['name'] === "CastError" ) {
+                res.status(402).send({
+                    status: 402,
+                    error: err,
+                    msg: `Error: '${err['value']}' is invalid id`
+                });
+            }
+            else
+            res.status(500).send({
+                status: 500,
+                msg: 'Error: Server Error',
+                error: err
+            });
         });
 };
