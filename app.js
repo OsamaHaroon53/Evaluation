a632422c1ad442fde0e241a725f5adb194c7c42e
OsamaHaroon53 <sonijaosama@gmail.com>
2018-12-29 17:54:48 +0500

    Changes for production

--- app.js
+++ app.js
@@ -5,24 +5,24 @@ const errorHandler = require('errorhandler');
 const database = require('./services/configDB');
 const routes = require('./routes/api');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(logger('dev'));
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Evaluation system');
 });
 
-app.use('/api',routes);
+app.use('/api', routes);
 
 // error handling middleware should be loaded after the loading the routes
 if (app.get('env') === 'development') {
     app.use(errorHandler())
 }
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`listening on port ${port}`)
 });
--- services/configDB.js
+++ services/configDB.js
@@ -2,7 +2,16 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/Evaluation', {
+var mongoURL;
+
+if (process.env.NODE_ENV === 'production') {
+    mongoURL = "mongodb://osama:tabish1@ds127139.mlab.com:27139/karachevaluationsystem";
+}
+else {
+    mongoURL = "mongodb://localhost:27017/Evaluation"
+}
+
+mongoose.connect(mongoURL, {
     useNewUrlParser: true,
     useCreateIndex: true
 });
