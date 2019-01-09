50b5ac816046658cd1274345c7d24686f1a807fa
OsamaHaroon53 <sonijaosama@gmail.com>
2019-01-09 20:23:56 +0500

    add validation and refactoring

--- routes/modules/account/signup.js
+++ routes/modules/account/signup.js
@@ -2,52 +2,26 @@ const _ = require('lodash');
 const Admin = require('../../../models/Admin');
 const Teacher = require('../../../models/Teacher');
 const Student = require('../../../models/Student');
+const Joi = require('joi');
 
 module.exports = async function (req, res, next) {
-    var role = req.body.role;
-    req.body.password = Math.random().toString(36).substring(5);
-    if (role === 'admin') {
-        let admin = await Admin.findOne(_.pick(req.body, "email"))
-        if (admin) {
-            return res.status(400).send({
-                status: 400,
-                msg: "Admin already exist"
-            });
-        }
-        admin = _.pick(req.body, ['email', 'name']);
-        admin.password = req.body.password;
-        role = new Admin(admin);
-    }
-    else if (role === 'teacher') {
-        let teacher = await Teacher.findOne(_.pick(req.body, "email"))
-        if (teacher) {
-            return res.status(400).send({
-                status: 400,
-                msg: "Teacher already exist"
-            });
-        }
-        teacher = _.pick(req.body, ['email', 'name']);
-        teacher.password = req.body.password;
-        role = new Teacher(teacher);
-    }
-    else if (role === 'student') {
-        let student = await Student.findOne(_.pick(req.body, "email"))
-        if (student) {
-            return res.status(400).send({
-                status: 400,
-                msg: "Student already exist"
-            });
-        }
-        student = _.pick(req.body, ['email', 'form_no']);
-        student.password = req.body.password;
-        role = new Student(student);
-    }
-    else {
-        return res.status(400).send({
+    var error = await validate(req.body);
+    if (error) {
+        return res.status(402).send({
             status: 402,
-            msg: "role is Invalid or empty"
+            error: error,
+            msg: "Validation Error"
         });
     }
+    req.body.password = Math.random().toString(36).substring(5);
+    var role = await uniqueEmail(req.body);
+    if(!role){
+        return res.status(400).send({
+            status: 400,
+            msg: "Email already exist"
+        });
+    }
+
     // save record
 
     await role.save()
@@ -59,4 +33,46 @@ module.exports = async function (req, res, next) {
         req.url = "/admin/sendpassword";
         next();
     }
+}
+
+async function uniqueEmail(body) {
+    var record = null;
+    if (body.role === 'admin') {
+        record = await Admin.findOne(_.pick(body,'email'));
+        if (record) {
+            return null;
+        }
+        record = _.pick(body, ['email', 'name','password']);
+        record = new Admin(record);
+    }
+    else if (body.role === 'teacher') {
+        record = await Teacher.findOne(_.pick(body,'email'));
+        if(record){
+            return null;
+        }
+        record = _.pick(body, ['email', 'name','password']);
+        record = new Teacher(record);
+    }
+    else if (body.role === 'student') {
+        record = await Student.findOne(_.pick(body,'email'));
+        if(record){
+            return null;
+        }
+        record = _.pick(body, ['email', 'form_no','password']);
+        record = new Admin(record);
+    }
+    return record;
+}
+
+async function validate(payload){
+        const schema = Joi.object().keys({
+            email: Joi.string().email().required(),
+            form_no: Joi.number(),
+            role: Joi.string().valid(['admin','teacher','student']).required(),
+            name: Joi.string()
+        }).xor('form_no','name');
+        let { error } = Joi.validate(payload, schema);
+        if(error !== null)
+            delete error['isJoi'];
+        return error;
 }
