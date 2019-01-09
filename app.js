97675eec0f4cef1edf9fa29614411bc1fe154111
OsamaHaroon53 <sonijaosama@gmail.com>
2019-01-09 16:13:38 +0500

    remove nodemailer api and add cors

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const errorHandler = require('errorhandler');
+const cors =require('cors');
 const database = require('./services/configDB');
 const routes = require('./routes/api');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(logger('dev'));
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,41 +25,6 @@ if (app.get('env') === 'development') {
     app.use(errorHandler())
 }
 
-var nodemailer = require("nodemailer");
-
-/*
-    Here we are configuring our SMTP Server details.
-    STMP is mail server which is responsible for sending and recieving email.
-*/
-var smtpTransport = nodemailer.createTransport({
-    service: "gmail",
-    host: "smtp.gmail.com",
-    auth: {
-        user: "ubitad436@gmail.com",
-        pass: "asdf9876;l"
-    }
-});
-/*------------------SMTP Over-----------------------------*/
-
-/*------------------Routing Started ------------------------*/
-
-app.post('/send', function (req, res) {
-    var mailOptions = {
-        to: req.body.to,
-        subject: req.body.subject,
-        text: req.body.text
-    }
-    console.log(mailOptions);
-    smtpTransport.sendMail(mailOptions, function (error, response) {
-        if (error) {
-            console.log(error);
-            res.end("error");
-        } else {
-            console.log("Message sent: " + response.message);
-            res.end("sent");
-        }
-    });
-});
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
