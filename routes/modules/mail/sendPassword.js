60a51b01622b15fd1c4488c8a2f505564ab2daa7
OsamaHaroon53 <sonijaosama@gmail.com>
2019-01-09 18:41:14 +0500

    ReFactoring

--- routes/modules/mail/sendPassword.js
+++ routes/modules/mail/sendPassword.js
@@ -12,6 +12,7 @@ const _ = require('lodash');
 var smtpTransport = nodemailer.createTransport({
     service: "gmail",
     host: "smtp.gmail.com",
+    secureConnection: true,
     auth: {
         user: "",
         pass: ""
@@ -22,7 +23,7 @@ module.exports = async function (req, res) {
 
     var role = req.body.role;
     if (!req['signUp']) {
-        req.body.password = await getPassword(role,_.pick(req.body, "email"));
+        req.body.password = await getPassword(role, _.pick(req.body, "email"));
         if (!req.body.password) {
             return res.status(400).send({
                 status: 400,
@@ -39,55 +40,32 @@ module.exports = async function (req, res) {
         let mail = await smtpTransport.sendMail(mailOptions);
         console.log(mailOptions, mail);
         if (mail) {
-
-            if (role === 'admin') {
-                let admin = await Admin.findOneAndUpdate(_.pick(req.body, "email"), { isActive: 'sent' })
-                if (admin) {
-                    return res.status(200).send({
-                        status: 200,
-                        msg: "signUp and mail send successfully",
-                        data: _.pick(req.body.data, 'email', 'name', 'isActive')
-                    });
-                }
-                return res.status(200).send("signUp and mail send successfully");
-            }
-            else if (role === 'teacher') {
-                let teacher = await Teacher.findOneAndUpdate(_.pick(req.body, "email"), { isActive: 'sent' })
-                if (teacher) {
-                    return res.status(400).send({
-                        status: 200,
-                        msg: "signUp and mail send successfully",
-                        data: _.pick(req.body.data, 'email', 'name', 'isActive')
-                    });
-                }
-            }
-            else if (role === 'student') {
-                let student = await Student.findOneAndUpdate(_.pick(req.body, "email"), { isActive: 'sent' })
-                if (student) {
-                    return res.status(400).send({
-                        status: 200,
-                        msg: "signUp and mail send successfully",
-                        data: _.pick(req.body, 'email', 'form_no', 'isActive')
-                    });
-                }
-            }
-            else {
+            let msg = req['signUp'] ? "signUp and mail send successfully" : "mail send successfully";
+            let record = await Update(role, _.pick(req.body, "email"));
+            if (record) {
                 return res.status(200).send({
                     status: 200,
-                    msg: "mail send successfully",
-                    data: _.pick(req.body, 'email')
+                    msg: msg,
+                    data: _.pick(record, 'email', 'name', 'form_no', 'isActive')
+                });
+            }
+            else {
+                return res.status(400).send({
+                    status: 500,
+                    msg: "Email has been sent but record not updated",
+                    data: _.pick(req.body, 'email', 'name', 'form_no')
                 });
             }
         }
         else {
-            if (req['signUp']){
+            if (req['signUp']) {
                 res.status(200).send({
                     status: 200,
                     msg: "signUp Succesfully and mail Not send",
-                    data: _.pick(req.body.data, 'email', 'isActive')
+                    data: _.pick(req.body.data, 'email', 'name', 'form_no')
                 });
             }
-            else{
+            else {
                 res.status(400).send({
                     status: 400,
                     msg: "mail Not send",
@@ -95,27 +73,18 @@ module.exports = async function (req, res) {
                 });
             }
         }
-        //     , function (error, response) {
-        //     if (error) {
-        //         console.log(error);
-        //         res.end("error");
-        //     } else {
-        //         console.log("Message sent: " + response.message);
-        //         res.end("sent");
-        //     }
-        // });
     }
     catch (ex) {
         console.log(ex)
-        if (req['signUp']){
+        if (req['signUp']) {
             res.status(200).send({
                 status: 500,
                 msg: "signUp Succesfully and mail Not send",
-                data: _.pick(req.body.data, 'email', 'isActive'),
+                data: _.pick(req.body.data, 'email'),
                 error: ex
             });
         }
-        else{
+        else {
             res.status(500).send({
                 status: 500,
                 msg: "server Error",
@@ -125,7 +94,7 @@ module.exports = async function (req, res) {
     }
 };
 
-async function getPassword(role,email) {
+async function getPassword(role, email) {
     var record;
     if (role === 'admin') {
         record = await Admin.findOne(email)
@@ -140,5 +109,22 @@ async function getPassword(role,email) {
         return record['password'];
     }
     return false;
+}
+
+async function Update(role, email) {
+    var record = null;
+    if (role === 'admin') {
+        record = await Admin.findOneAndUpdate(email, { isActive: 'sent' })
+    }
+    else if (role === 'teacher') {
+        record = await Teacher.findOneAndUpdate(email, { isActive: 'sent' })
+    }
+    else if (role === 'student') {
+        record = await Student.findOneAndUpdate(email, { isActive: 'sent' })
+    }
+    if (record) {
+        record.isActive = 'sent';
+    }
+    return record;
 }
 
