024e8b6a7a8e83731a19877e935cf3effed81a2a
OsamaHaroon53 <sonijaosama@gmail.com>
2019-01-15 18:26:45 +0500

    admin Authorization

---
+++ middleware/admin.js
@@ -0,0 +1,11 @@
+module.exports = function(req,res,next){
+    if(req.user.role === 1){
+        next();
+    }
+    else{
+        return res.status(403).send({
+            msg: "Permission denied. you are not Admin",
+            status: 403
+        });
+    }
+}
