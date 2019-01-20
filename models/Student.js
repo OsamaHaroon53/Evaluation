382ce87f290517342d14232a6803c9cbcd46899e
OsamaHaroon53 <sonijaosama@gmail.com>
2019-01-20 17:46:49 +0500

    Update model for email and password

--- models/Student.js
+++ models/Student.js
@@ -33,11 +33,25 @@ const studentSchema = new Schema({
     },
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true,
+        validate: {
+            isAsync: true,
+            validator: function (v, cb) {
+                setTimeout(function () {    
+                    var valid = (/^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i.test(v));
+                    var msg = v + ' is not a valid email number!';
+                    cb(valid, msg);
+                }, 5);
+            },
+            message: 'Request Time out'
+        }
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        minlength: 5,
+        maxlength: 12
     },
     img:{
         type: String
--- models/Teacher.js
+++ models/Teacher.js
@@ -28,11 +28,24 @@ const teacherSchema = new Schema({
     email:{
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        validate: {
+            isAsync: true,
+            validator: function (v, cb) {
+                setTimeout(function () {    
+                    var valid = (/^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i.test(v));
+                    var msg = v + ' is not a valid email number!';
+                    cb(valid, msg);
+                }, 5);
+            },
+            message: 'Request Time out'
+        }
     },
     password:{
         type: String,
-        required: true
+        required: true,
+        minlength: 5,
+        maxlength: 12
     },
     img:{
         type: String
