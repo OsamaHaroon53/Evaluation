430fb840dd06e6d96889e9403fc012cb738f86db
OsamaHaroon53 <sonijaosama@gmail.com>
2019-02-13 14:45:46 +0500

    update get program api for better response

--- routes/modules/program/getPrograms.js
+++ routes/modules/program/getPrograms.js
@@ -1,17 +1,32 @@
 const Program = require('../../../models/Program');
 
-module.exports = async function (req, res, next) {
-    var data = await Program.find().select('-__v');
-    if(!data.length){
-        return res.status(200).send({
-            status: 304,
-            msg: "Programs Not Found"
+module.exports = async function (req, res) {
+    await Program.aggregate([
+        {
+            $group: {
+                _id: "$program",
+                semesters: { $addToSet: { semesterNo: "$semester", _id: "$_id" } }
+            }
+        },
+        {
+            $addFields: { program: "$_id" }
+        },
+        {
+            $project: { _id: 0 }
+        }
+    ], function (err, result) {
+        if (err) {
+            res.status(500).send({
+                status: 500,
+                msg: "Server Error",
+                error: err
+            })
+            return;
+        }
+        res.status(200).send({
+            status: 200,
+            msg: "Get Programs Succesfully",
+            data: result
         });
-    }
-    
-    res.status(200).send({
-        status: 200,
-        msg: "Get Programs Succesfully",
-        data: data
     });
 }
