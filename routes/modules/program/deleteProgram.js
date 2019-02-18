65888f7d8ed45da3612f33fc7924da6e472b3332
OsamaHaroon53 <sonijaosama@gmail.com>
2019-02-19 04:18:34 +0500

    Delete API updated

--- routes/modules/program/deleteProgram.js
+++ routes/modules/program/deleteProgram.js
@@ -1,21 +1,33 @@
 const Program = require('../../../models/Program');
+const { validateDelete: validate } = require('../Validation/program');
 
 module.exports = async (req, res) => {
-    await Program.findByIdAndDelete(req.params.id)
-        .then(data => {
-            data ? res.status(200).send({
-                status: 200,
-                msg: 'Program Delete succesfully'
-            }) : res.send({
-                status: 304,
-                msg: 'Program not found'
-            });
-        })
-        .catch(err => {
-            res.status(500).send({
-                status: 500,
-                msg: 'Error: Server Error',
-                error: err
-            });
+    var { body } = req;
+    var error = await validate(body);
+    if (error) {
+        return res.status(402).send({
+            status: 402,
+            error: error,
+            msg: "Validation Error"
         });
+    }
+    record = await Program.deleteMany(body);
+    if(record['ok'] == 1 && !record['n']){
+        return res.status(400).send({
+            status: 400,
+            msg: "Program Not Found"
+        });
+    }
+    if(record['ok'] == 1 && record['n']){
+        return res.status(200).send({
+            status: 200,
+            msg: "Program Delete Successfully",
+            data: record
+        });
+    }
+    res.status(500).send({
+        status: 500,
+        msg: "Server Error",
+        error: record
+    });
 };
